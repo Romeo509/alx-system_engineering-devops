64697f550b5ee5cfc09b6048d0fd922f3fdd588e
Romeo509 <romeoameku503@gmail.com>
2024-03-25 13:37:07 +0000

    testing the api

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -1,40 +1,29 @@
 #!/usr/bin/python3
-import sys
+"""Fetches employee's information and TODO list from a REST API."""
+import json
 import requests
+import sys
+
+base_url = 'https://jsonplaceholder.typicode.com'
 
 if __name__ == "__main__":
-    if len(sys.argv) != 2 or not sys.argv[1].isdigit():
-        print("Usage: python3 0-gather_data_from_an_API.py <employee_id>")
-        sys.exit(1)
+    """Script to fetch employee info and their TODO list from an API."""
+    employee_id = sys.argv[1]
 
-    employee_id = int(sys.argv[1])
-
-    # Fetching user data
-    user_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/users/{employee_id}"
-    )
-    todo_response = requests.get(
-        f"https://jsonplaceholder.typicode.com/todos?userId={employee_id}"
-    )
-
-    if user_response.status_code != 200:
-        print("Error: Employee data not found")
-        sys.exit(1)
-    if todo_response.status_code != 200:
-        print("Error: TODO data not found")
-        sys.exit(1)
-
+    user_url = f"{base_url}/users/{employee_id}"
+    user_response = requests.get(user_url)
     user_data = user_response.json()
-    todo_data = todo_response.json()
+    user_name = user_data['name']
 
-    # Filter completed tasks
-    completed_tasks = [task for task in todo_data if task['completed']]
+    tasks_url = f"{base_url}/todos?userId={employee_id}"
+    tasks_response = requests.get(tasks_url)
+    tasks_data = tasks_response.json()
 
-    # Displaying results
-    print((
-        f"Employee {user_data['name']} is done with tasks"
-        f"({len(completed_tasks)}/{len(todo_data)}):"
-    ))
+    completed_tasks = [task for task in tasks_data if task['completed']]
 
+    print(
+         f"Employee {user_name} is done with tasks "
+        f"({len(completed_tasks)}/{len(tasks_data)}):"
+    )
     for task in completed_tasks:
-        print(f"\t {task['title']}")
+        print(f"\t{task['title']}")
