0b5a7f00631b2c7a3087224cf213f2a0ac60c933
Romeo509 <romeoameku503@gmail.com>
2024-04-09 23:28:45 +0000

    reddit:  first 10 hot posts

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -17,7 +17,7 @@ def number_of_subscribers(subreddit):
     Returns:
         int: The number of subscribers, or 0 if the subreddit is invalid.
     """
-    headers = {'User-Agent': 'MyBot/1.0'}  # Setting a custom User-Agent
+    headers = {'User-Agent': 'MyBot/1.0'}
     url = f'https://www.reddit.com/r/{subreddit}/about.json'
     response = requests.get(url, headers=headers, allow_redirects=False)
 
---
+++ 0x16-api_advanced/1-top_ten.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""
+Queries the Reddit API and prints the titles
+of the first 10 hot posts listed for a given subreddit.
+"""
+
+import requests
+
+
+def top_ten(subreddit):
+    """
+    Prints the titles of the first 10 hot posts for a given subreddit.
+
+    Args:
+        subreddit (str): The name of the subreddit.
+
+    Returns:
+        None
+    """
+    headers = {'User-Agent': 'MyBot/1.0'}
+    url = f'https://www.reddit.com/r/{subreddit}/hot.json'
+    params = {'limit': 10}
+    response = requests.get(url, headers=headers, params=params,
+                            allow_redirects=False)
+
+    if response.status_code == 200:
+        data = response.json()
+        posts = data['data']['children']
+        for post in posts:
+            print(post['data']['title'])
+    else:
+        print(None)
+
+
+if __name__ == '__main__':
+    import sys
+    if len(sys.argv) < 2:
+        print("Please pass an argument for the subreddit to search.")
+    else:
+        top_ten(sys.argv[1])
